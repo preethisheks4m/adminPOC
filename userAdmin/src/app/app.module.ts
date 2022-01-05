5001e639c9343ee706b00b54e6ff4fda5f5be302
preethisheks4m <85828602+preethisheks4m@users.noreply.github.com>
2022-01-05 16:01:03 +0530

    initial commit

--- userAdmin/src/app/app.module.ts
+++ userAdmin/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule
